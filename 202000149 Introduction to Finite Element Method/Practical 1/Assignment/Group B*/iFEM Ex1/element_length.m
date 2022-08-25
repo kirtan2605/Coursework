e6005d3acad1f9c7aa02a0e93b424aa38722bcf0
Kirtan Patel <59549172+kirtan2605@users.noreply.github.com>
2022-08-25 16:54:14 +0530

    Delete 202000149 Introduction to Finite Element Method directory

--- 202000149 Introduction to Finite Element Method/Practical 1/Assignment/Group B*/iFEM Ex1/element_length.m
+++
@@ -1,3 +0,0 @@
-function L = element_length(Ni, Nj)
-% Ni, Nj = coordinates node i and node j [x,y]
-L = sqrt( (Nj(1)-Ni(1))^2 + (Nj(2)-Ni(2))^2 ); % length element
--- 202000149 Introduction to Finite Element Method/Practical 1/Assignment/Group B*/iFEM Ex1/k_local.m
+++
@@ -1,7 +0,0 @@
-function [K] = k_local( E, A, Ni, Nj )
-% E = Elasticity modulus
-% A = Cross-section area
-% Ni, Nj = coordinates node i and node j [x,y]
-L = sqrt( (Nj(1)-Ni(1))^2 + (Nj(2)-Ni(2))^2 ); % length element
-B = 1/L * [-1,1];
-K = A*L* B'*E*B;
