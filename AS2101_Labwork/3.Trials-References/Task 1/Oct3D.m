23aac18cdb42af1ec85cf67a9edf65eb0b1ea067
Kirtan Patel <59549172+kirtan2605@users.noreply.github.com>
2023-02-10 02:04:53 +0530

    Delete AS2101_Labwork directory

--- AS2101_Labwork/3.Trials-References/Task 1/Oct3D.m
+++
@@ -1,9 +0,0 @@
-tx = ty = linspace (-8, 8, 41)';
-[x, y] = meshgrid (tx, ty);
-r = sqrt (x.^ 2 + y.^ 2);
-tz = sin (r) ./ r;
-mesh (tx, ty, tz);
-xlabel ("tx");
-ylabel ("ty");
-zlabel ("tz");
-title ("3D Plot");
--- AS2101_Labwork/3.Trials-References/Task 3/Submission Codes/Secant Method/secm_main.py
+++
@@ -1,6 +0,0 @@
-from secm import *
-
-N_list, f_values = secant(a = -3, b = 0, e = 0.001, N_max = 15)
-
-print(N_list)
-print(f_values)
